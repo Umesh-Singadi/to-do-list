80a395bb6a40acf5147d2757cec72d63e31375ba
Umesh Singadi <umeshsingadi007@gmail.com>
2024-01-02 21:02:49 +0530

    Rendering Tasks

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,21 @@ import { useSelector } from "react-redux";
 function App() {
   const tasks = useSelector((state) => state.tasks.list);
 
-  return <h1>App</h1>;
+  return (
+    <h1>
+      <div className="p-10">
+        <ul className="flex flex-col border rounded p-5">
+          {tasks.map((task) => (
+            <ls key={task.id} className="m-1 border rounded-md p-2">
+              <input type="checkbox" value={task.completed} />
+              <label className="m-3">{task.text}</label>
+              <button className="border p-1 rounded">Remove</button>
+            </ls>
+          ))}
+        </ul>
+      </div>
+    </h1>
+  );
 }
 
 export default App;
