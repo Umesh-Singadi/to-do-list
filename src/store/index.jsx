6dafe8825858e2b903e41a032f6f80238be866ec
Umesh Singadi <umeshsingadi007@gmail.com>
2024-01-03 10:44:53 +0530

    Creating seperate slices

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,58 +1,23 @@
 import { createSlice, configureStore } from "@reduxjs/toolkit";
+import {
+  tasksReducer,
+  addTask,
+  removeTask,
+  setTaskValue,
+  toggleTask,
+} from "./slices/tasksSlice";
 
-const tasksSlice = createSlice({
-  name: "tasks",
-  initialState: {
-    taskValue: "",
-    list: [
-      { id: 1, text: "Umesh", completed: false },
-      { id: 2, text: "Ajit", completed: false },
-      { id: 3, text: "Niranjan", completed: false },
-    ],
-  },
-  reducers: {
-    setTaskValue(state, action) {
-      state.taskValue = action.payload;
-    },
-    removeTask(state, action) {
-      state.list = state.list.filter((task) => task.id !== action.payload);
-    },
-    addTask(state, action) {
-      state.list.push({
-        id: Date.now(),
-        text: state.taskValue,
-        competed: false,
-      }),
-        (state.taskValue = "");
-    },
-    toggleTask(state, action) {
-      const task = state.list.find((task) => task.id === action.payload);
-      if (task) {
-        task.completed = !task.completed;
-      }
-    },
-  },
-});
-
-const visibilityFilterSlice = createSlice({
-  name: "visibilityFilter",
-  initialState: "all",
-  reducers: {
-    setVisibilityFilter(state, action) {
-      return action.payload;
-    },
-  },
-});
+import {
+  visibilityFilterReducer,
+  setVisibilityFilter,
+} from "./slices/visibilityFilterSlice";
 
 const store = configureStore({
   reducer: {
-    tasks: tasksSlice.reducer,
-    visibilityFilter: visibilityFilterSlice.reducer,
+    tasks: tasksReducer,
+    visibilityFilter: visibilityFilterReducer,
   },
 });
 
 export { store };
-export const { toggleTask, removeTask, addTask, setTaskValue } =
-  tasksSlice.actions;
-
-export const { setVisibilityFilter } = visibilityFilterSlice.actions;
+export { toggleTask, removeTask, addTask, setTaskValue, setVisibilityFilter };
