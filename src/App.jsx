39d0a887348f9c8fd73b79e4cf9e731fcee03469
Umesh Singadi <umeshsingadi007@gmail.com>
2024-01-03 10:32:37 +0530

    Adding form element to applicaton

--- src/App.jsx
+++ src/App.jsx
@@ -27,60 +27,62 @@ function App() {
         return tasks;
     }
   };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
   return (
-    <div>
-      <div className="p-10">
-        <h1>Tasks List</h1>
-        <div>
-          <label>
-            Show :{" "}
-            <select
-              className="border"
-              onChange={(e) => dispatch(setVisibilityFilter(e.target.value))}
-            >
-              <option value="all">All</option>
-              <option value="active">Active</option>
-              <option value="completed">Completed</option>
-            </select>
-          </label>
-        </div>
-        <div className="m-5">
-          <label>
-            New Task :
-            <input
-              type="text"
-              className="border p-1 rounded"
-              value={taskValue}
-              onChange={handleChange}
-            />
-          </label>
-          <button
-            className="border p-1 rounded"
-            onClick={() => dispatch(addTask())}
+    <form className="p-10" onSubmit={handleSubmit}>
+      <h1>Tasks List</h1>
+      <div>
+        <label>
+          Show :{" "}
+          <select
+            className="border"
+            onChange={(e) => dispatch(setVisibilityFilter(e.target.value))}
           >
-            Add Task
-          </button>
-        </div>
-        <ul className="flex flex-col border rounded p-5">
-          {filteredTask().map((task) => (
-            <li key={task.id} className="m-1 border rounded-md p-2">
-              <input
-                type="checkbox"
-                value={task.completed}
-                onChange={() => dispatch(toggleTask(task.id))}
-              />
-              <label className="m-3">{task.text}</label>
-              <button
-                onClick={() => dispatch(removeTask(task.id))}
-                className="border p-1 rounded"
-              >
-                Remove
-              </button>
-            </li>
-          ))}
-        </ul>
+            <option value="all">All</option>
+            <option value="active">Active</option>
+            <option value="completed">Completed</option>
+          </select>
+        </label>
       </div>
-    </div>
+      <div className="m-5">
+        <label>
+          New Task :
+          <input
+            type="text"
+            className="border p-1 rounded"
+            value={taskValue}
+            onChange={handleChange}
+          />
+        </label>
+        <button
+          className="border p-1 rounded"
+          onClick={() => dispatch(addTask())}
+        >
+          Add Task
+        </button>
+      </div>
+      <ul className="flex flex-col border rounded p-5">
+        {filteredTask().map((task) => (
+          <li key={task.id} className="m-1 border rounded-md p-2">
+            <input
+              type="checkbox"
+              value={task.completed}
+              onChange={() => dispatch(toggleTask(task.id))}
+            />
+            <label className="m-3">{task.text}</label>
+            <button
+              onClick={() => dispatch(removeTask(task.id))}
+              className="border p-1 rounded"
+            >
+              Remove
+            </button>
+          </li>
+        ))}
+      </ul>
+    </form>
   );
 }
 
